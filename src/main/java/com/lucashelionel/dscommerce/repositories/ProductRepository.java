946870b36d6f5cfe7c4aee5a730c1e2ff93ba9e9
Lucas Helionel <48215665+lucashelionel@users.noreply.github.com>
2024-02-19 19:25:43 -0300

    arquitetura de 3 camadas e teste da API

---
+++ src/main/java/com/lucashelionel/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.lucashelionel.dscommerce.repositories;
+
+import com.lucashelionel.dscommerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+}
