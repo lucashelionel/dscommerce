ed0036d99ca659fd074525350e36dcb947aecb17
Lucas Helionel <48215665+lucashelionel@users.noreply.github.com>
2024-02-21 19:41:42 -0300

    get by id
    implementação de DTOs e Services para recuperar um elemento com base no seu id

--- src/main/java/com/lucashelionel/dscommerce/controllers/ProductController.java
+++ src/main/java/com/lucashelionel/dscommerce/controllers/ProductController.java
@@ -1,9 +1,12 @@
 package com.lucashelionel.dscommerce.controllers;
 
+import com.lucashelionel.dscommerce.dto.ProductDTO;
 import com.lucashelionel.dscommerce.entities.Product;
 import com.lucashelionel.dscommerce.repositories.ProductRepository;
+import com.lucashelionel.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +16,11 @@ import java.util.Optional;
 @RequestMapping(value = "/products")
 public class ProductController {
     @Autowired
-    private ProductRepository repository;
-    @GetMapping
-    public String teste(){
-        Optional<Product> result = repository.findById(1L);
-        Product product = result.get();
-        return product.getName();
+    private ProductService service;
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+        ProductDTO dto = service.findById(id);
+        return dto;
+        // ou simplemente: return service.findById(id);
     }
 }
---
+++ src/main/java/com/lucashelionel/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,53 @@
+package com.lucashelionel.dscommerce.dto;
+
+import com.lucashelionel.dscommerce.entities.Product;
+import jakarta.persistence.Column;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private String imgUrl;
+
+    public ProductDTO(){
+
+    }
+
+    public ProductDTO(Long id, String name, String description, double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        description = entity.getDescription();
+        price = entity.getPrice();
+        imgUrl = entity.getImgUrl();
+    }
+
+    //só tem os geterrs no DTO pois a intenção dele é só expor os dados sem alterações no banco
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
