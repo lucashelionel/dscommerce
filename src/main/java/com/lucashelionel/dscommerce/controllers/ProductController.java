ac020031801bd21a6f0778427f07eb0691e5fd9b
Lucas Helionel <48215665+lucashelionel@users.noreply.github.com>
2024-02-23 22:23:57 -0300

    inserindo um produto
    criação dos métodos de inserção de um produto no controller e na classe service no banco por meio do método POST e

--- src/main/java/com/lucashelionel/dscommerce/controllers/ProductController.java
+++ src/main/java/com/lucashelionel/dscommerce/controllers/ProductController.java
@@ -7,10 +7,7 @@ import com.lucashelionel.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,5 +28,11 @@ public class ProductController {
     @GetMapping
     public Page<ProductDTO> findAll(Pageable pageable){
         return service.findAll(pageable);
+    }
+
+    @PostMapping
+    public ProductDTO insert(@RequestBody ProductDTO dto) // para indicar que é o corpo da requisição
+    {
+        return service.insert(dto);
     }
 }
