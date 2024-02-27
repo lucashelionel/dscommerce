ecf9beafb5877fa61e59ff38387dfd9e326044c1
Lucas Helionel <48215665+lucashelionel@users.noreply.github.com>
2024-02-27 20:47:34 -0300

    Atualizando um produto
    implementação do método de requisição HTTP PUT, para atualizar um produto

--- src/main/java/com/lucashelionel/dscommerce/services/ProductService.java
+++ src/main/java/com/lucashelionel/dscommerce/services/ProductService.java
@@ -33,14 +33,28 @@ public class ProductService {
 
     @Transactional //sem o read only pois é uma operação de salvar no banco
     public ProductDTO insert(ProductDTO dto){
+        //esse metodo de salvamento segue essa ordem: instanciou, copiou e salvou
         Product entity = new Product();
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto){
+        // diferentes dos anteriores ele não vai diretamente no banco de dados
+        //esse metodo de salvamento segue essa ordem: instanciou com a referência, copiou e salvou
+        Product entity = repository.getReferenceById(id);
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
-
-        entity = repository.save(entity);
-
-        return new ProductDTO(entity);
     }
 }
